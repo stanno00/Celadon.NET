ccede59561514e5a10b5bdf81d4589906b6f1f63
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-07 17:54:11 +0100

    Added one-to-one relationship

--- DotNetTribes/Models/User.cs
+++ DotNetTribes/Models/User.cs
@@ -6,5 +6,7 @@
         public string UserName { get; set; }
         public string HashedPassword { get; set; }
         
+        public int? KingdomId { get; set; }
+        public Kingdom Kingdom { get; set; }
     }
 }

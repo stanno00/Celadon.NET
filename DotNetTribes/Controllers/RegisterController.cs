18afb71997a66de807bb5311e3a18694628f4606
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-07 16:07:02 +0100

    added blank controller class for user registration

---
+++ DotNetTribes/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTribes.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegisterController
+    {
+        
+        // TODO: change the return type (ActionResult?)
+        // TODO: add service (+ its interface) for registration
+        [HttpPost]
+        public void RegisterNewUser()
+        {
+            
+        }
+    }
+}

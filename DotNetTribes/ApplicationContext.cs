02d041ad373812792174597d320edd33d56013cf
RadekKocka <kocka.radek@gmail.com>
2022-03-07 14:40:36 +0100

    initial commit

---
+++ DotNetTribes/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTribes
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}

a3123ec88703bd692f33d6ab86a32ce25ac27d31
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-07 17:45:56 +0100

    Added User model

---
+++ DotNetTribes/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetTribes.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string HashedPassword { get; set; }
+        
+    }
+}

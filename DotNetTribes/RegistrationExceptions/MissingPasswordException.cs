f0763383e86f94196cac19dfc8cda8c12f994b63
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-08 12:46:04 +0100

    Added exception for missing field for password

---
+++ DotNetTribes/RegistrationExceptions/MissingPasswordException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetTribes.RegistrationExceptions
+{
+    public class MissingPasswordException : Exception
+    {
+        public MissingPasswordException() : this("Password is required.")
+        {
+        }
+
+        public MissingPasswordException(string message) : base(message)
+        {
+        }
+    }
+}

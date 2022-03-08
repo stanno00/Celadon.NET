a950c5570ac151bf9d7605dd96b0f7daf860698f
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-08 12:56:54 +0100

    added exception for handling missing username

---
+++ DotNetTribes/RegistrationExceptions/MissingUsernameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetTribes.RegistrationExceptions
+{
+    public class MissingUsernameException : Exception
+    {
+        public MissingUsernameException() : this("Username is required.")
+        {
+        }
+
+        public MissingUsernameException(string message) : base(message)
+        {
+        }
+    }
+}

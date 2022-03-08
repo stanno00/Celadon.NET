55f5349154ab2309bfd35aa73f7ba47cef859b9d
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-08 11:41:10 +0100

    changes in User model class

--- DotNetTribes/Models/User.cs
+++ DotNetTribes/Models/User.cs
@@ -3,10 +3,11 @@
     public class User
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string HashedPassword { get; set; }
+        public string Email { get; set; }
         
-        public int? KingdomId { get; set; }
+        public int KingdomId { get; set; }
         public Kingdom Kingdom { get; set; }
     }
 }

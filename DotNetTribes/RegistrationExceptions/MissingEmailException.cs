47de90c59bd134515879dab37dbc4ce1559c64e3
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-08 12:54:08 +0100

    added exception for handling missing email

---
+++ DotNetTribes/RegistrationExceptions/MissingEmailException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetTribes.RegistrationExceptions
+{
+    public class MissingEmailException : Exception
+    {
+        public MissingEmailException() : this("Email is required.")
+        {
+        }
+
+        public MissingEmailException(string message) : base(message)
+        {
+        }
+    }
+}

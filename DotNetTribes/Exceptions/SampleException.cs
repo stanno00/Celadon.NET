79772a11227e73e4e33fd6f278a4be29b9aeff37
YanariYoukai <BeautifulNightmare1992>
2022-03-08 17:28:51 +0100

    Implemented global error handling, added documentation and a sample custom exception.

---
+++ DotNetTribes/Exceptions/SampleException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetTribes.Exceptions
+{
+    public class SampleException : Exception
+    {
+        public SampleException() : base ("This is a sample exception.")
+        {
+        }
+
+        public SampleException(string yourParameter) : base($"This is a sample message using {yourParameter}.")
+        {
+        }
+    }
+}

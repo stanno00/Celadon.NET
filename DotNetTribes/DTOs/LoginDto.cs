58f67ab8b9c29fbceaec3186d5475601c910dfbc
stanno00 <stanno00@gmail.com>
2022-03-09 10:32:53 +0100

    Created dto for incoming and outcoming json

---
+++ DotNetTribes/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace DotNetTribes.DTOs
+{
+    public class LoginDto
+    {
+        public string status { get; set; }
+        public string token { get; set; }
+    }
+}
---
+++ DotNetTribes/DTOs/UsernamePasswordDto.cs
@@ -0,0 +1,8 @@
+namespace DotNetTribes.DTOs
+{
+    public class UsernamePassowrdDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}

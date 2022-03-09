92067db57a19f28f08a82b080e0cd1373bed02b5
stanno00 <stanno00@gmail.com>
2022-03-09 11:14:10 +0100

    add endpoint for login

--- DotNetTribes/Controllers/LoginController.cs
+++ DotNetTribes/Controllers/LoginController.cs
@@ -1,3 +1,6 @@
+using System;
+using DotNetTribes.DTOs;
+using DotNetTribes.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetTribes.Controllers
@@ -6,6 +9,26 @@ namespace DotNetTribes.Controllers
     [Route("[controller]")]
     public class LoginController
     {
-        
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromBody] UsernamePassowrdDto usernamePassowrdDto)
+        {
+            try
+            {
+                var token = _loginService.VerifiUsernameAndPassword(usernamePassowrdDto);
+                return new CreatedResult("", token);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new BadRequestResult();
+            }
+        }
     }
 }

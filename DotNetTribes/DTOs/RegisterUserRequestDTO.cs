113b9dc8b4bf204e84dcf5684fd181a7f5d9235e
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-11 09:17:57 +0100

    minor changes

--- DotNetTribes/DTOs/RegisterUserRequestDTO.cs
+++ DotNetTribes/DTOs/RegisterUserRequestDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace DotNetTribes.DTOs
+﻿using Newtonsoft.Json;
+
+namespace DotNetTribes.DTOs
 {
     public class RegisterUserRequestDTO
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        
+        // comment from Alex: "There is no need to set an empty string as a default value"
         public string Kingdomname { get; set; } = "";
         public string Email { get; set; }
         

871704ed72c998e0ec372eb7507bea3e638411c6
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-03-11 10:22:47 +0100

    removed default value for Kingdomname property

--- DotNetTribes/DTOs/RegisterUserRequestDTO.cs
+++ DotNetTribes/DTOs/RegisterUserRequestDTO.cs
@@ -1,15 +1,10 @@
-﻿using Newtonsoft.Json;
-
-namespace DotNetTribes.DTOs
+﻿namespace DotNetTribes.DTOs
 {
     public class RegisterUserRequestDTO
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        
-        // comment from Alex: "There is no need to set an empty string as a default value"
-        public string Kingdomname { get; set; } = "";
+        public string? Kingdomname { get; set; }
         public string Email { get; set; }
-        
     }
 }

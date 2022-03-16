2eb59bba6ab0bd39c25579f3c7c7271f864f4da7
YanariYoukai <BeautifulNightmare1992>
2022-03-16 10:52:59 +0100

    implemented saving changes to the database

--- DotNetTribes/Services/KingdomService.cs
+++ DotNetTribes/Services/KingdomService.cs
@@ -20,14 +20,13 @@ namespace DotNetTribes.Service
 
         public KingdomDto KingdomInfo(int kingdomId)
         {
-
             IdIsNullOrDoesNotExist(kingdomId);
-            
+
             var kingdom = _applicationContext.Kingdoms
-                    .Include(k => k.Buildings)
-                    .Include(k => k.Resources)
-                    .Include(k => k.Troops)
-                    .Single(k => k.KingdomId == kingdomId);
+                .Include(k => k.Buildings)
+                .Include(k => k.Resources)
+                .Include(k => k.Troops)
+                .Single(k => k.KingdomId == kingdomId);
 
             KingdomDto kingdomDto = new KingdomDto()
             {
@@ -37,86 +36,55 @@ namespace DotNetTribes.Service
                 Resources = kingdom.Resources,
                 Troops = kingdom.Troops
             };
-            
+
             return kingdomDto;
         }
 
         public BuildingResponseDTO CreateNewBuilding(int kingdomId, BuildingRequestDTO request)
         {
-            /* actual value
-            var kingdom = _applicationContext.Kingdoms
-                    .Include(b => b.Buildings)
-                    .FirstOrDefault(k => k.KingdomId == kingdomId);
-                    
-                    var kingdomGold = kingdom.Resources.FirstOrDefault(r => r.Type == ResourceType.Gold); */
+            IdIsNullOrDoesNotExist(kingdomId);
             
-            //dummy kingdom
-            var user = new User
-            {
-                UserId = 1,
-                Username = "Yanari",
-                Email = "j@k.cz",
-            };
-
-            var kingdom = new Kingdom
-            {
-                KingdomId = 1,
-                Name = "Yanariland",
-                Buildings = new List<Building>(),
-                Resources = new List<Resource>(),
-                
-
-            };
-
-            kingdom.User = user;
-            user.Kingdom = kingdom;
-            kingdom.Resources.Add(new Resource
-            {
-                Amount = 500,
-                Kingdom = kingdom,
-                KingdomId = 1,
-                ResourceId = 1,
-                Type = ResourceType.Gold
-            });
+            var kingdom = _applicationContext.Kingdoms
+                .Include(b => b.Buildings)
+                .Include(r => r.Resources)
+                .FirstOrDefault(k => k.KingdomId == kingdomId);
 
             var kingdomGold = kingdom.Resources.FirstOrDefault(r => r.Type.Equals(ResourceType.Gold));
-            int buildingPrice = 1000;
-            BuildingType btypes;
-
+            
+            //TODO: remove this after Rules get implemented
+            int buildingPrice = 100;
 
             if (request.Type == null)
             {
                 throw new BuildingCreationException("Building type required.");
             }
-            
-            if (!Enum.TryParse(request.Type, true, out btypes))
+
+            if (!Enum.TryParse(request.Type, true, out BuildingType requestedBuilding))
             {
                 throw new BuildingCreationException("Incorrect building type.");
             }
-            
+
             if (buildingPrice > kingdomGold.Amount)
             {
                 throw new BuildingCreationException("Gold needed.");
             }
 
-            var requested = btypes;
-
-
             Building toBeAdded = new Building
-                {
-                    //placeholder values
-                    Type = requested,
-                    Level = 1,
-                    Hp = 100,
-                    Started_at = 1000000,
-                    Finished_at = 1500000,
-                    KingdomId = kingdomId,
-                    BuildingId = 1 
-                };
-            
+            {
+                //TODO: these are placeholder values, update after Rules get implemented
+                Type = requestedBuilding,
+                Level = 1,
+                Hp = 100,
+                Started_at = 1000000,
+                Finished_at = 1500000,
+                KingdomId = kingdomId,
+                BuildingId = 1
+            };
+
             kingdomGold.Amount -= buildingPrice;
             kingdom.Buildings.Add(toBeAdded);
-            
+            _applicationContext.SaveChanges();
+
             return new BuildingResponseDTO
             {
                 Type = toBeAdded.Type.ToString(),
@@ -127,13 +95,13 @@ namespace DotNetTribes.Service
                 Id = toBeAdded.BuildingId
             };
         }
-        
+
         private void IdIsNullOrDoesNotExist(int kingdomId)
         {
             if (kingdomId == 0 || _applicationContext.Kingdoms
                     .Last().KingdomId > kingdomId)
             {
-               throw new KingdomDoesNotExistException();
+                throw new KingdomDoesNotExistException();
             }
         }
     }

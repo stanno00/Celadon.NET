9573ec8f5d91bfb5e6d2459bab86a1c2a14d4f96
YanariYoukai <BeautifulNightmare1992>
2022-03-17 10:53:15 +0100

    Created the first version of rules implementation using a service

---
+++ DotNetTribes/Services/ProductionRulesService.cs
@@ -0,0 +1,101 @@
+﻿namespace DotNetTribes.Services
+{
+    public class ProductionRulesService: IRulesService
+    {
+        public int StartingGold = 500;
+        
+        public int CalculateTownhallPrice(int level)
+        {
+            return level * 200;
+        }
+        
+        public int CalculateFarmPrice(int level)
+        {
+            return level * 100;
+        }
+        
+        public int CalculateMinePrice(int level)
+        {
+            return level * 100;
+        }
+        
+        public int CalculateAcademyPrice(int level)
+        {
+            if (level == 1)
+            {
+                return 150;
+            }
+            return level * 100;
+        }
+        
+        public int CalculateTroopPrice(int level)
+        {
+            return level * 25;
+        }
+        
+        public int CalculateTownhallHP(int level)
+        {
+            return level * 200;
+        }
+        
+        public int CalculateFarmHP(int level)
+        {
+            return level * 100;
+        }
+        
+        public int CalculateMineHP(int level)
+        {
+            return level * 100;
+        }
+        
+        public int CalculateAcademyHP(int level)
+        {
+            return level * 150;
+        }
+        
+        public int CalculateTroopHp(int level)
+        {
+            return level * 20;
+        }
+        
+        public int CalculateTownhallBuildingTime(int level)
+        {
+            if (level == 1)
+            {
+                return 120;
+            }
+
+            return level * 60;
+        }
+        
+        public int CalculateFarmBuildingTime(int level)
+        {
+            return level * 60;
+        }
+        
+        public int CalculateMineBuildingTime(int level)
+        {
+            return level * 60;
+        }
+        
+        public int CalculateAcademyBuildingTime(int level)
+        {
+            if (level == 1)
+            {
+                return 90;
+            }
+
+            return level * 60;
+        }
+        
+        public int CalculateTroopBuildingTime(int level)
+        {
+            return level * 30;
+        }
+        
+        
+        
+        
+        
+    }
+}
